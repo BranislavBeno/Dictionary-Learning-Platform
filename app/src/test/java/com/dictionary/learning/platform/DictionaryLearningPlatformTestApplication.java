503aefac86e1f30a500bd076644329bbbbe960dc
Branislav Beno <brano.beno@gmail.com>
2024-11-10 22:07:42 +0100

    fix: Stop initialize application context twice

--- app/src/test/java/com/dictionary/learning/platform/DictionaryLearningPlatformTestApplication.java
+++ app/src/test/java/com/dictionary/learning/platform/DictionaryLearningPlatformTestApplication.java
@@ -2,9 +2,7 @@ package com.dictionary.learning.platform;
 
 import com.dictionary.learning.platform.config.ContainersConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DictionaryLearningPlatformTestApplication {
 
     public static void main(String[] args) {
