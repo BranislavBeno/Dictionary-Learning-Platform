835040321a6983bca43f12ed6f1fc68d6ea4a9cb
Branislav Beno <Branislav.Beno.ext@msg.group>
2024-11-11 15:24:03 +0100

    feat: Make some repository methods pageable

--- app/src/main/java/com/dictionary/learning/platform/word/WordRepository.java
+++ app/src/main/java/com/dictionary/learning/platform/word/WordRepository.java
@@ -1,6 +1,8 @@
 package com.dictionary.learning.platform.word;
 
 import java.util.Set;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,25 +10,25 @@ public interface WordRepository extends JpaRepository<Word, Long> {
 
     @Query(
             """
-             SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
-             FROM Word w
-             LEFT JOIN w.user u
-             WHERE u.id = :id""")
-    Set<WordDto> findAllByUserId(long id);
+         SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
+         FROM Word w
+         LEFT JOIN w.user u
+         WHERE u.id = :id""")
+    Page<WordDto> findAllByUserId(Pageable pageable, long id);
 
     @Query(
             """
-             SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
-             FROM Word w
-             LEFT JOIN w.user u
-             WHERE u.id = :id AND w.grade = :grade""")
+         SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
+         FROM Word w
+         LEFT JOIN w.user u
+         WHERE u.id = :id AND w.grade = :grade""")
     Set<WordDto> findAllByUserIdByGrade(long id, int grade);
 
     @Query(
             """
-             SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
-             FROM Word w
-             LEFT JOIN w.user u
-             WHERE u.id = :id AND w.grade = :grade AND w.lesson = :lesson""")
+         SELECT new com.dictionary.learning.platform.word.WordDto(w.id, w.en, w.sk, w.lesson, w.grade)
+         FROM Word w
+         LEFT JOIN w.user u
+         WHERE u.id = :id AND w.grade = :grade AND w.lesson = :lesson""")
     Set<WordDto> findAllByUserIdByGradeByLesson(long id, int grade, int lesson);
 }
--- app/src/test/java/com/dictionary/learning/platform/word/WordRepositoryTest.java
+++ app/src/test/java/com/dictionary/learning/platform/word/WordRepositoryTest.java
@@ -1,44 +1,47 @@
 package com.dictionary.learning.platform.word;
 
-import com.dictionary.learning.platform.repository.BaseTestRepository;
-import java.util.List;
 import java.util.Set;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
+import com.dictionary.learning.platform.repository.BaseTestRepository;
 
 @Sql(scripts = "/sql/init_db.sql")
 class WordRepositoryTest extends BaseTestRepository implements WithAssertions {
 
-    @Autowired
-    private WordRepository repository;
+   @Autowired
+   private WordRepository repository;
 
-    @Test
-    void testFindAll() {
-        List<Word> words = repository.findAll();
+   @ParameterizedTest
+   @CsvSource({ "0,5", "1,4" })
+   void testFindAll(int pageNumber, int expected) {
+      Page<Word> words = repository.findAll(PageRequest.of(pageNumber, 5));
+      assertThat(words).hasSize(expected);
+   }
 
-        assertThat(words).hasSize(9);
-    }
+   @ParameterizedTest
+   @CsvSource({ "0,5", "1,2" })
+   void testFindAllByUserId(int pageNumber, int expected) {
+      Page<WordDto> words = repository.findAllByUserId(PageRequest.of(pageNumber, 5), 1);
+      assertThat(words).hasSize(expected);
+   }
 
-    @Test
-    void testFindAllByUserId() {
-        Set<WordDto> words = repository.findAllByUserId(1);
+   @Test
+   void testFindAllByUserIdByGrade() {
+      Set<WordDto> words = repository.findAllByUserIdByGrade(1, 2);
 
-        assertThat(words).hasSize(7);
-    }
+      assertThat(words).hasSize(3);
+   }
 
-    @Test
-    void testFindAllByUserIdByGrade() {
-        Set<WordDto> words = repository.findAllByUserIdByGrade(1, 2);
+   @Test
+   void testFindAllByUserIdByGradeByLesson() {
+      Set<WordDto> words = repository.findAllByUserIdByGradeByLesson(1, 2, 2);
 
-        assertThat(words).hasSize(3);
-    }
-
-    @Test
-    void testFindAllByUserIdByGradeByLesson() {
-        Set<WordDto> words = repository.findAllByUserIdByGradeByLesson(1, 2, 2);
-
-        assertThat(words).hasSize(1);
-    }
+      assertThat(words).hasSize(1);
+   }
 }
