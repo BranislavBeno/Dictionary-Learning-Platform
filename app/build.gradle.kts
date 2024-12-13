19da6be93a5086ad43b4abd6f12e4b158f0d2294
Branislav Beno <branislav.beno@siemens.com>
2024-12-13 15:34:24 +0100

    chore: Increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ apply(plugin = "io.spring.dependency-management")
 group = "com.dictionary.learning.platform"
 
 val versionMajor = 0
-val versionMinor = 1
+val versionMinor = 2
 val versionPatch = 0
 
 version = "v$versionMajor.$versionMinor.$versionPatch"
